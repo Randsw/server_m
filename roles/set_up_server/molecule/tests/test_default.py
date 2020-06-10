b1922be8923c844452ce86a379460766c3f00eff
randsw <randsw@mail.ru>
2020-06-10 15:59:40 +0300

    Change stderr to stdout in set_up_server test. Restructed variables in roles

--- roles/set_up_server/molecule/tests/test_default.py
+++ roles/set_up_server/molecule/tests/test_default.py
@@ -7,30 +7,30 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 cmd = host.run('jython --version')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'Jython 2') > -1
+    assert cmd.stdout.find(u'Jython 2') > -1
 
 
 def test_mc_is_installed(host):
     cmd = host.run('mc --version')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'GNU Midnight Commander') > -1
+    assert cmd.stdout.find(u'GNU Midnight Commander') > -1
 
 def test_git_is_installed(host):
     cmd = host.run('git --version')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'git version') > -1
+    assert cmd.stdout.find(u'git version') > -1
 
 def test_htop_is_installed(host):
     cmd = host.run('git --version')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'htop') > -1
+    assert cmd.stdout.find(u'htop') > -1
 
 def test_ncdu_is_installed(host):
     cmd = host.run('ncdu --version')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'ncdu') > -1
+    assert cmd.stdout.find(u'ncdu') > -1
 
 def test_nload_is_installed(host):
     cmd = host.run('nload --help')
     assert cmd.rc == 0
-    assert cmd.stderr.find(u'nload version') > -1
+    assert cmd.stdout.find(u'nload version') > -1
