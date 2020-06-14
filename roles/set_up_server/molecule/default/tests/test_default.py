0e6b6a217b66f8fed04efa0324aefa4fadcb5981
Randsw <randsw@mail.ru>
2020-06-14 19:02:42 +0300

    Add set_up_network role. Set up ip address using netplan Ubuntu. Correct set_up_ftp_role

--- roles/set_up_server/molecule/default/tests/test_default.py
+++ roles/set_up_server/molecule/default/tests/test_default.py
@@ -5,11 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-cmd = host.run('jython --version')
-    assert cmd.rc == 0
-    assert cmd.stdout.find(u'Jython 2') > -1
-
-
 def test_mc_is_installed(host):
     cmd = host.run('mc --version')
     assert cmd.rc == 0
@@ -21,12 +16,12 @@ def test_git_is_installed(host):
     assert cmd.stdout.find(u'git version') > -1
 
 def test_htop_is_installed(host):
-    cmd = host.run('git --version')
+    cmd = host.run('htop --version')
     assert cmd.rc == 0
     assert cmd.stdout.find(u'htop') > -1
 
 def test_ncdu_is_installed(host):
-    cmd = host.run('ncdu --version')
+    cmd = host.run('ncdu -v')
     assert cmd.rc == 0
     assert cmd.stdout.find(u'ncdu') > -1
 
