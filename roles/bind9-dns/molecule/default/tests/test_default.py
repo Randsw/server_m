e020d10124ae63dd16ed746d76f0c7745e3b6f85
Randsw <randsw@mail.ru>
2020-07-12 20:02:39 +0300

    Add more test to bind9-dns role. Add "failed_when" condition to restart bind9 handler

--- roles/bind9-dns/molecule/default/tests/test_default.py
+++ roles/bind9-dns/molecule/default/tests/test_default.py
@@ -5,12 +5,27 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_git_is_installed(host):
+def test_bins_is_working(host):
     cmd = host.run('docker exec bind service named status')
     assert cmd.rc == 0
     assert cmd.stdout.find(u'running') > -1
 
-#def test_git_is_installed(host):
-#    cmd = host.run('docker exec bind service named status')
-#    assert cmd.rc == 0
-#    assert cmd.stdout.find(u'running') > -1
+def test_server_is_resolved(host):
+    cmd = host.run('docker exec bind nslookup dep7server.com 172.18.0.2')
+    assert cmd.rc == 0
+    assert cmd.stdout.find(u'192.168.1.142') > -1
+
+def test_mailserver_is_resolved(host):
+    cmd = host.run('docker exec bind nslookup mail.dep7server.com 172.18.0.2')
+    assert cmd.rc == 0
+    assert cmd.stdout.find(u'192.168.1.142') > -1
+
+def test_reposerver_is_resolved(host):
+    cmd = host.run('docker exec bind nslookup repo.dep7server.com 172.18.0.2')
+    assert cmd.rc == 0
+    assert cmd.stdout.find(u'192.168.1.144') > -1
+
+def test_jenkinsserver_is_resolved(host):
+    cmd = host.run('docker exec bind nslookup jenkins.dep7server.com 172.18.0.2')
+    assert cmd.rc == 0
+    assert cmd.stdout.find(u'192.168.1.143') > -1
