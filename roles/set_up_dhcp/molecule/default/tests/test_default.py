e45f2971c396b342301e6fa1324adbd1ba888ee4
Randsw <randsw@mail.ru>
2020-07-18 21:30:13 +0300

    Add systemd role. That role convert docker-compose file to linux systemd daemon. First try - rocketchat. Fix some minor bugs

--- roles/set_up_dhcp/molecule/default/tests/test_default.py
+++ roles/set_up_dhcp/molecule/default/tests/test_default.py
@@ -9,7 +9,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_dhcp_is_running(host):
     assert host.service('isc-dhcp-server').is_running
 
-def test_ssh_pub_key(host):
+def test_dhcp_config_exist(host):
     dhcp_conf = host.file('/etc/dhcp/dhcpd.conf')
     assert dhcp_conf.exists
     assert dhcp_conf.contains('option domain-name-servers 172.17.0.2')
---
+++ roles/systemd/molecule/default/tests/test_default.py
@@ -0,0 +1,20 @@
+import os
+import testinfra.utils.ansible_runner
+
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+def test_rocket_is_running(host):
+    assert host.service('RocketChat').is_running
+
+def test_rocketchar_container_is_running(host):
+    rocketchat = host.docker("root_rocketchat_1")
+    assert rocketchat.is_running
+
+def test_mongodb_container_is_running(host):
+    mongodb = host.docker("root_mongo_1")
+    assert mongodb.is_running
+
+def test_listening_service(host):
+    assert host.socket("tcp://0.0.0.0:3000").is_listening
