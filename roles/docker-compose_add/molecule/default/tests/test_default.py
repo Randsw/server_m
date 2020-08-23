cc0280d32405d00cc1b82ed1b86a9c12e6bc7eba
Randsw <randsw@mail.ru>
2020-08-23 17:35:54 +0300

    Verify docker-compose add role

--- roles/docker-compose_add/molecule/default/tests/test_default.py
+++ roles/docker-compose_add/molecule/default/tests/test_default.py
@@ -6,45 +6,65 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 def test_webmail_container_is_running(host):
-    webmail = host.docker("mail_server_webmail_1")
+    webmail = host.docker("docker_compose_webmail_1")
     assert webmail.is_running
 
 def test_smtp_container_is_running(host):
-    smtp = host.docker("mail_server_smtp_1")
+    smtp = host.docker("docker_compose_smtp_1")
     assert smtp.is_running
 
 def test_imap_container_is_running(host):
-    imap = host.docker("mail_server_imap_1")
+    imap = host.docker("docker_compose_imap_1")
     assert imap.is_running
 
 def test_antispam_container_is_running(host):
-    antispam = host.docker("mail_server_antispam_1")
+    antispam = host.docker("docker_compose_antispam_1")
     assert antispam.is_running
 
 def test_admin_container_is_running(host):
-    admin = host.docker("mail_server_admin_1")
+    admin = host.docker("docker_compose_admin_1")
     assert admin.is_running
 
 def test_webdav_container_is_running(host):
-    webdav = host.docker("mail_server_webdav_1")
+    webdav = host.docker("docker_compose_webdav_1")
     assert webdav.is_running
 
 def test_database_container_is_running(host):
-    database = host.docker("mail_server_database_1")
+    database = host.docker("docker_compose_database_1")
     assert database.is_running
 
 def test_fetchmail_container_is_running(host):
-    fetchmail = host.docker("mail_server_fetchmail_1")
+    fetchmail = host.docker("docker_compose_fetchmail_1")
     assert fetchmail.is_running
 
 def test_antivirus_container_is_running(host):
-    antivirus = host.docker("mail_server_antivirus_1")
+    antivirus = host.docker("docker_compose_antivirus_1")
     assert antivirus.is_running
 
 def test_redis_container_is_running(host):
-    redis = host.docker("mail_server_redis_1")
+    redis = host.docker("docker_compose_redis_1")
     assert redis.is_running
 
+def test_listening_http(host):
+    assert host.socket("tcp://0.0.0.0:80").is_listening
 
-def test_listening_service(host):
-    assert host.socket("tcp://0.0.0.0:3000").is_listening
+def test_listening_https(host):
+    assert host.socket("tcp://0.0.0.0:443").is_listening
+
+def test_listening_imap(host):
+    assert host.socket("tcp://0.0.0.0:143").is_listening
+
+def test_listening_pop3(host):
+    assert host.socket("tcp://0.0.0.0:110").is_listening
+
+def test_listening_pop3s(host):
+    assert host.socket("tcp://0.0.0.0:993").is_listening
+
+def test_listening_imaps(host):
+    assert host.socket("tcp://0.0.0.0:995").is_listening
+
+def test_listening_startls(host):
+    assert host.socket("tcp://0.0.0.0:465").is_listening
+
+def test_listening_smtps(host):
+    assert host.socket("tcp://0.0.0.0:587").is_listening
--- roles/systemd/molecule/default/tests/test_default.py
+++ roles/systemd/molecule/default/tests/test_default.py
@@ -16,26 +16,5 @@ def test_mongodb_container_is_running(host):
     mongodb = host.docker("root_mongo_1")
     assert mongodb.is_running
 
-def test_listening_http(host):
-    assert host.socket("tcp://0.0.0.0:80").is_listening
-
-def test_listening_https(host):
-    assert host.socket("tcp://0.0.0.0:443").is_listening
-
-def test_listening_imap(host):
-    assert host.socket("tcp://0.0.0.0:143").is_listening
-
-def test_listening_pop3(host):
-    assert host.socket("tcp://0.0.0.0:110").is_listening
-
-def test_listening_pop3s(host):
-    assert host.socket("tcp://0.0.0.0:993").is_listening
-
-def test_listening_imaps(host):
-    assert host.socket("tcp://0.0.0.0:995").is_listening
-
-def test_listening_startls(host):
-    assert host.socket("tcp://0.0.0.0:487").is_listening
-
-def test_listening_smtps(host):
-    assert host.socket("tcp://0.0.0.0:587").is_listening
+def test_listening_service(host):
+    assert host.socket("tcp://0.0.0.0:3000").is_listening
